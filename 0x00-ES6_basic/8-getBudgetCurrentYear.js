d829c87ff748b1956febd2b333eee445864bfd80
lonebhen <ben.angmortey@gmail.com>
2023-01-31 09:01:08 +0000

    8

---
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,16 @@
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const budget = {};
+
+  budget[`income-${getCurrentYear()}`] = income;
+  budget[`gdp-${getCurrentYear()}`] = gdp;
+  budget[`capita-${getCurrentYear()}`] = capita;
+
+  return budget;
+}
+
+console.log(getBudgetForCurrentYear(2100, 5200, 1090));
