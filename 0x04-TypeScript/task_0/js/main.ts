40a8bf3fce1d85b88e541aa56889ecbddcfa8f0a
lonebhen <ben.angmortey@gmail.com>
2023-02-02 18:02:01 +0000

    main

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -19,6 +19,28 @@ const student_2 : Student = {
     location: "Cape Coast"
 }
 
-const studentsList : Array<Student> = [student_1,student_2]
+const studentsList : Array<Student> = [student_1,student_2];
 
-console.log(studentsList.length)
+const body:HTMLBodyElement = document.getElementsByTagName("body")[0];
+const table : HTMLTableElement = document.createElement("table");
+const thead: HTMLTableSectionElement = document.createElement("thead");
+const tbody :HTMLTableSectionElement = document.createElement("tbody");
+const rowHead : HTMLTableRowElement = thead.insertRow(0);
+const cell1Head: HTMLTableCellElement = rowHead.insertCell(0);
+const cell2Head: HTMLTableCellElement = rowHead.insertCell(1);
+
+cell1Head.innerHTML = "firstName";
+cell2Head.innerHTML = "location";
+table.append(thead);
+
+studentsList.forEach((student) => {
+  const row: HTMLTableRowElement = tbody.insertRow(0);
+  const cell1: HTMLTableCellElement = row.insertCell(0);
+  const cell2: HTMLTableCellElement = row.insertCell(1);
+
+  cell1.innerHTML = student.firstName;
+  cell2.innerHTML = student.location;
+});
+
+table.append(tbody);
+body.append(table);
