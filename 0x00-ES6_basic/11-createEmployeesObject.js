b2ef1f44daaf764992e7e46db881a1d1d5a5532e
lonebhen <ben.angmortey@gmail.com>
2023-02-15 10:45:43 +0000

    11

---
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -0,0 +1,8 @@
+export default function createEmployeesObject(departmentName, employees) {
+  return {
+    [`${departmentName}`]:
+      employees,
+  };
+}
+
+console.log(createEmployeesObject('Software', ['Bob', 'Sylvie']));
